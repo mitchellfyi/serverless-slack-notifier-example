776bcdb1a73009f78c492f6ad182a7f5d9fe3479
Mitchell <198377+mitchell-fyi@users.noreply.github.com>
2023-03-06 17:30:58 +0000

    initial event handler

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
+"use strict";
+
+const axios = require("axios");
+
+const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  if (!slackWebhookUrl || !event?.body) return;
+
+  try {
+    const requestBody = JSON.parse(event.body);
+  } catch {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Not valid JSON" }),
+    };
+  }
+
+  if (requestBody.Type === "SpamNotification") {
+    const to = requestBody.Email;
+    const from = requestBody.From;
+    const message = `New spam notification for ${email}`;
+
+    await axios.post(slackWebhookUrl, {
+      text: message,
+    });
+
+    try {
+      const response = await axios.post(slackWebhookUrl, { text: message });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Alert sent to Slack" }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "There was a problem sending the message to Slack",
+        }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "No action taken" }),
+    };
+  }
 };
